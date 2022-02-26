ef97906a0fbbeee85d1bf65d6079441551be3612
Bondhan Mondal <bondhan2725@gmail.com>
2022-02-27 03:27:20 +0600

    26-8 Money withdraw and reduce balance for withdraw

--- js/banking.js
+++ js/banking.js
@@ -32,4 +32,36 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     // clear the deposit input field
     depositInput.value = '';
     
+});
+
+//handle withdraw event handler
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    //get Withdraw Input
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawInputText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawInputText);
+
+    //set withdraw total
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawAmount = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawAmount + newWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    //update Account Balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+
+
+
+    //clear withdraw input field
+    withdrawInput.value = '';
 })
